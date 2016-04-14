b3d4e075264f3a8d9f8d4d563cc64e33e634e172
Otto Bergdal <ottbe754@mux2-14.edu.isy.liu.se>
2016-04-14 15:09:24 +0200

    modified portmap to gpu to support sprite pos

--- copter_modern.vhd
+++ copter_modern.vhd
@@ -20,6 +20,8 @@ architecture Behavioral of copter_modern is
   port (  clk : in std_logic; --system clock
           cpu_x : in std_logic_vector(9 downto 0);
           cpu_y : in std_logic_vector(8 downto 0);
+          sprite_x : in integer;
+          sprite_y : in integer;
           cpu_data : in std_logic_vector(7 downto 0);
           collision : out std_logic);
    
@@ -29,6 +31,9 @@ architecture Behavioral of copter_modern is
   signal cpu_x : std_logic_vector(9 downto 0);
   signal cpu_y : std_logic_vector(8 downto 0);
   signal cpu_data : std_logic_vector(7 downto 0);
+  signal sprite_x : integer;
+  signal sprite_y : integer;
+   
   
 begin  -- Behavioral
 
@@ -38,6 +43,8 @@ begin  -- Behavioral
   GP : GPU port map (clk => clk,
                      cpu_x => cpu_x,
                      cpu_y => cpu_y,
+                     sprite_x => sprite_x,
+                     sprite_y => sprite_y,
                      cpu_data => cpu_data,
                      collision => collision);
  
