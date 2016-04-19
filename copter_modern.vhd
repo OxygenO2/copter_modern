92f8d276946e7daa37d42e36786baa235d2eae89
Sebastian Callh <sebca553@mux2-11.edu.isy.liu.se>
2016-04-19 11:35:08 +0200

    changed signal names

--- copter_modern.vhd
+++ copter_modern.vhd
@@ -4,29 +4,47 @@ use IEEE.STD_LOGIC_ARITH.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity copter_modern is
-  port (EXP-IO_P<0> : out std_logic;
-        EXP-IO_P<1> : out std_logic;
-        EXP-IO_P<2> : out std_logic;
-        EXP-IO_P<3> : out std_logic;
-        EXP-IO_P<4> : out std_logic;
-        EXP-IO_P<5> : out std_logic;
-        EXP-IO_P<6> : out std_logic;
-        EXP-IO_P<7> : out std_logic;
-        EXP-IO_P<8> : out std_logic;
-        EXP-IO_P<9> : out std_logic;
-        EXP-IO_P<10> : out std_logic;
-        EXP-IO_P<11> : out std_logic;
-        EXP-IO_P<12> : out std_logic;
-        EXP-IO_P<13> : out std_logic;
-        EXP-IO_P<14> : out std_logic;
-        EXP-IO_P<15> : out std_logic;
-        EXP-IO_P<16> : out std_logic;
-        EXP-IO_P<17> : out std_logic;
-        EXP-IO_P<18> : out std_logic;
-        EXP-IO_P<19> : out std_logic;
-        EXP-IO_P<20> : out std_logic);
+  port(clk : in std_logic;
+      TP_BUSY : out std_logic;
+      TP_DIN : out std_logic;
+      TP_CS : out std_logic;
+      TP_DCLK : out std_logic;
+      TP_DOUT : out std_logic;
+      TP_PENIRQ : out std_logic;
 
+      TFT_CLK : out std_logic;
+      TFT_DISP : out std_logic;
+      TFT_EN : out std_logic;
+      TFT_DE : out std_logic;
+      LED_EN : out std_logic;
 
+      TFT_R0 : out std_logic;
+      TFT_R1 : out std_logic;
+      TFT_R2 : out std_logic;
+      TFT_R3 : out std_logic;
+      TFT_R4 : out std_logic;
+      TFT_R5 : out std_logic;
+      TFT_R6 : out std_logic;
+      TFT_R7 : out std_logic;
+
+      TFT_B0 : out std_logic;
+      TFT_B1 : out std_logic;
+      TFT_B2 : out std_logic;
+      TFT_B3 : out std_logic;
+      TFT_B4 : out std_logic;
+      TFT_B5 : out std_logic;
+      TFT_B6 : out std_logic;
+      TFT_B7 : out std_logic;
+
+      TFT_G0 : out std_logic;
+      TFT_G1 : out std_logic;
+      TFT_G2 : out std_logic;
+      TFT_G3 : out std_logic;
+      TFT_G4 : out std_logic;
+      TFT_G5 : out std_logic;
+      TFT_G6 : out std_logic;
+      TFT_G7 : out std_logic);
+        
 end copter_modern;
 
 architecture Behavioral of copter_modern is
@@ -34,8 +52,7 @@ architecture Behavioral of copter_modern is
   component CPU
     port (
       clk : in std_logic;
-      collision : in std_logic);
-    
+      collision : in std_logic);      
   end component;
 
   component GPU
@@ -54,51 +71,10 @@ architecture Behavioral of copter_modern is
   signal cpu_y : std_logic_vector(8 downto 0);
   signal cpu_data : std_logic_vector(7 downto 0);
 
-  signal TP_BUSY : std_logic;
-  signal TP_DIN : std_logic;
-  signal TP_CS : std_logic;
-  signal TP_DCLK : std_logic;
-  signal TP_DOUT : std_logic;
-  signal TP_PENIRQ : std_logic;
 
-  signal TFT_CLK : std_logic;
-  signal TFT_DISP : std_logic;
-  signal TFT_EN : std_logic;
-  signal TFT_DE : std_logic;
-  signal LED_EN : std_logic;
-
- 
   signal sprite_x : integer;
   signal sprite_y : integer;
    
-  
-  signal TFT_B[7] : std_logic;
-  signal TFT_B[6] : std_logic;
-  signal TFT_B[5] : std_logic;
-  signal TFT_B[4] : std_logic;
-  signal TFT_B[3] : std_logic;
-  signal TFT_B[2] : std_logic;
-  signal TFT_B[1] : std_logic;
-  signal TFT_B[0] : std_logic;
-
-  signal TFT_G[7] : std_logic;
-  signal TFT_G[6] : std_logic;
-  signal TFT_G[5] : std_logic;
-  signal TFT_G[4] : std_logic;
-  signal TFT_G[3] : std_logic;
-  signal TFT_G[2] : std_logic;
-  signal TFT_G[1] : std_logic;
-  signal TFT_G[0] : std_logic;
-
-  signal TFT_R[7] : std_logic;
-  signal TFT_R[6] : std_logic;
-  signal TFT_R[5] : std_logic;
-  signal TFT_R[4] : std_logic;
-  signal TFT_R[3] : std_logic;
-  signal TFT_R[2] : std_logic;
-  signal TFT_R[1] : std_logic;
-  signal TFT_R[0] : std_logic;
-
 begin  -- Behavioral
 
   CP : CPU port map (clk => clk,
@@ -119,5 +95,7 @@ begin  -- Behavioral
       end if;
    end process;
 
+
+    
       
     end Behavioral;
